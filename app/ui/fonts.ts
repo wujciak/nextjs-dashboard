ed9d08f2bd90fbc190fb67cf8c5de8d56ad9a1e4
Jakub <jakub.wojciak2002@gmail.com>
2025-08-26 00:19:56 +0200

    init commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+    subsets: ['latin']
+});
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'] // 400 normal, 700 bold
+});
